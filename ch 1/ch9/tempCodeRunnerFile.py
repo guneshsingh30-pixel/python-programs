2f8d843952630240087f5d05a2a1002ee140bd04
guneshsingh30 <guneshsingh30@gmail.com>
2026-06-20 23:52:49 +0530

    added file program1

---
+++ ch 1/ch9/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+import random
+def game():
+    print("You are playing the game!!")
+    score=random.randint(1,70)
+    with open("hiscore.txt","r") as f:
+        hiscore=f.read()
+        if(hiscore!=""):
+            hiscore=int(hiscore)
+        else:
+            hiscore=0
+    print(f"Your score:{score}")
+    if(score>hiscore):
+        with open ("hiscore.txt","w")as f:
+            f.write(str(score))
+    return score
+game()
